7436de873a9df04d98adfcaa069bbe9fd7a07a47
amigin <amigin@gmail.com>
2022-07-03 16:38:48 +0300

    Fixing UI

--- TypeScript/HtmlMain.ts
+++ TypeScript/HtmlMain.ts
@@ -36,7 +36,7 @@ class HtmlMain {
 
     public static generateMetrics(metrics: IMetrics): string {
 
-        let result = '<table class="table table-striped" style="font-size:10px"><tr><th>Started</th > <th>Duration < /th><th>Message</th > </tr>';
+        let result = '<table class="table table-striped" style="font-size:10px"><tr><th>Started</th><th>Duration</th><th>Message</th></tr>';
         for (let metric of metrics.metrics.sort((a, b) => b.started > a.started ? 1 : -1)) {
 
             let date = new Date(metric.started / 1000);
