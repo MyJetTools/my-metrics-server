6339a5948fae72b9a4c7de8cbaf921dcefeedacf
amigin <amigin@gmail.com>
2024-10-12 22:13:54 +0300

    Removed Debug info

--- src/flows/upload_events.rs
+++ src/flows/upload_events.rs
@@ -1,6 +1,5 @@
 use crate::{app_ctx::AppContext, db::*};
 
 pub async fn upload_events(app: &AppContext, events: Vec<MetricDto>) {
-    println!("Uploading events: {}", events.len());
     app.to_write_queue.enqueue(events).await;
 }
