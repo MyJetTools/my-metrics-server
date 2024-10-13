11a886ef37df2b285bc56074eb6e6c73655f3e56
amigin <amigin@gmail.com>
2024-10-13 12:14:45 +0300

    fixes with USER_ID_TAG

--- src/flows/upload_events.rs
+++ src/flows/upload_events.rs
@@ -1,6 +1,6 @@
 use crate::{app_ctx::AppContext, db::*};
 
-const CLIENT_ID_TAG: &str = "client_id";
+const USER_ID_TAG: &str = "user_id";
 
 pub async fn upload_events(app: &AppContext, mut events: Vec<MetricDto>) {
     populate_user_id(app, &mut events).await;
@@ -11,7 +11,7 @@ async fn populate_user_id(app: &AppContext, events: &mut Vec<MetricDto>) {
     let mut cache_write_access = app.cache.lock().await;
 
     for event in events {
-        if let Some(client_id) = event.get_tag_value(CLIENT_ID_TAG) {
+        if let Some(client_id) = event.get_tag_value(USER_ID_TAG) {
             cache_write_access
                 .process_id_user_id_links
                 .update(event.id, client_id);
@@ -20,7 +20,7 @@ async fn populate_user_id(app: &AppContext, events: &mut Vec<MetricDto>) {
                 .process_id_user_id_links
                 .resolve_user_id(event.id)
             {
-                event.add_tag(CLIENT_ID_TAG.to_string(), user_id.to_string());
+                event.add_tag(USER_ID_TAG.to_string(), user_id.to_string());
             }
         }
     }
