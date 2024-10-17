875e6dd47d3307a887911737d12d52d2daa4ca57
amigin <amigin@gmail.com>
2024-10-17 23:19:59 +0300

    Debugging remove process_id

--- src/process_id_user_id_links.rs
+++ src/process_id_user_id_links.rs
@@ -1,48 +1,42 @@
-use rust_extensions::{date_time::DateTimeAsMicroseconds, sorted_vec::*};
+use std::collections::HashMap;
+
+use rust_extensions::date_time::DateTimeAsMicroseconds;
 
 #[derive(Debug)]
 pub struct ProcessIdUserIdLink {
-    pub process_id: i64,
     pub user_id: String,
     pub created_at: DateTimeAsMicroseconds,
 }
 
-impl EntityWithKey<i64> for ProcessIdUserIdLink {
-    fn get_key(&self) -> &i64 {
-        &self.process_id
-    }
-}
-
 pub struct ProcessIdUserIdLinks {
-    pub items: SortedVec<i64, ProcessIdUserIdLink>,
+    pub items: HashMap<i64, ProcessIdUserIdLink>,
 }
 
 impl ProcessIdUserIdLinks {
     pub fn new() -> Self {
         Self {
-            items: SortedVec::new(),
+            items: HashMap::new(),
         }
     }
 
     pub fn update(&mut self, process_id: i64, user_id: &str) {
-        match self.items.insert_or_if_not_exists(&process_id) {
-            InsertIfNotExists::Insert(insert_entity) => {
-                insert_entity.insert(ProcessIdUserIdLink {
-                    process_id,
+        if !self.items.contains_key(&process_id) {
+            self.items.insert(
+                process_id,
+                ProcessIdUserIdLink {
                     user_id: user_id.to_string(),
                     created_at: DateTimeAsMicroseconds::now(),
-                });
-            }
-            InsertIfNotExists::Exists(_) => {}
+                },
+            );
         }
     }
 
     pub fn gc(&mut self) {
         let now = DateTimeAsMicroseconds::now();
         let mut to_remove = Vec::new();
-        for itm in self.items.iter() {
+        for (process_id, itm) in self.items.iter() {
             if (now - itm.created_at).get_full_seconds() >= 20 {
-                to_remove.push(itm.process_id);
+                to_remove.push(*process_id);
             }
         }
 
